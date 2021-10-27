82defb16652c4b27c3800a6662631c727406a180
DanB450x <91401342+DanB450x@users.noreply.github.com>
2021-10-27 18:48:04 +0200

    Add files via upload

---
+++ Jardin/AspersorON.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+
+servoPIN = 37
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p.start(7.5)#2.5
+try:
+  while True:
+    p.ChangeDutyCycle(14)
+    time.sleep(1)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(1)
+    #break;
+except KeyboardInterrupt:
+  p.stop()
+  #break;
+  GPIO.cleanup()
+
+#break;
